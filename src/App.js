9d8037260f9da39c99e7ced7ec6b74b2e7cab9d5
Michael Nelles <mnelles@overlayanalytics.com>
2021-03-06 12:41:50 -0500

    randomizer init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function makeid() {
+   var text = "";
+   var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+   for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+   return text;
+}
+
+const Block = (props) => {
+   const { id } = props;
+};
+const rand = () => {
+   return Math.floor(Math.random() * 10);
+};
+
+const diff = () => {
+   let ret = [];
+   for (let i = 0; i <= 100; i++) {
+      let id = makeid();
+      ret.push(
+         <div
+            key={"i-" + i}
+            style={{
+               display: "inline-block",
+               padding: 10,
+               textAlign: "center",
+               width: 50,
+               border: "1px solid #333",
+               margin: 3,
+               backgroundColor: "#f" + rand() + rand(),
+            }}
+         >
+            {id}
+         </div>
+      );
+   }
+   console.log(ret);
+   return ret;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   let t = diff();
+   return <div>{t.map((o) => o)}</div>;
 }
 
 export default App;
