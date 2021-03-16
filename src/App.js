a9dfe63cdfce830b4867029482680e73bc300502
Michael Nelles <mnelles@overlayanalytics.com>
2021-03-16 17:37:11 -0400

    error fix on import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import localForage from "localforage";
 
 import Snap from "./views/Snapshot";
 import Colors from "./views/Colors";
